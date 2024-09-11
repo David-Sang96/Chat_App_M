e0daaeadbc699a723e43e9385478d700d19c24c4
David-Sang96 <luainawl@gmail.com>
2024-09-11 14:25:27 +0800

    project init

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(5200, () => console.log(`Server is listing on PORT 5200`));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-red-400 sm:text-lime-300 lg:text-red-400">Hello</h1>
+    </>
+  );
+}
+
+export default App;
