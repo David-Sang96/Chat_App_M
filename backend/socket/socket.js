04643d3429add6a4845cb61d265e3dc9224ac1b8
David-Sang96 <luainawl@gmail.com>
2024-09-14 11:53:26 +0800

    update origin

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,13 +1,18 @@
+import dotenv from "dotenv";
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 
 const app = express();
+dotenv.config();
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://chat-app-c0yf.onrender.com"
+        : "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
